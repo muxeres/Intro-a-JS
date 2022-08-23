33cdeeda003a00895e1673e4e9871d5018708421
Karola Coquelicot <49755046+muxeres@users.noreply.github.com>
2022-08-23 11:10:58 -0400

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,17 @@
+// 3 formas para declarar variables
+// var count= 5;
+// let saludo = "Hola"; //responde solo donde fue declarado
+// const booleano = true; // variable reservada, no podrá variar en ningún punto
+
+// console.log(count + 10);
+// console.log(count + true);
+// booleano = false;
+
+// console.log(count = []);
+// saludo= 50.56;
+// console.log(saludo);
+
+function saludo(){
+    alert ("Hey, Cómo estás?!")
+    console.log("Hey, Cómo estás?!")
+}
